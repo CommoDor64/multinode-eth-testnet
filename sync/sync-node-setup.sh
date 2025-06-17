2f97d31314efbf3de7f4d2e518061604d94c5f2f
Dor Cohen <commo64dor@gmail.com>
2025-06-17 16:05:56 +0200

    move things around

--- sync/sync-node-setup.sh
+++ sync/sync-node-setup.sh
@@ -10,18 +10,8 @@ install_dependencies() {
     apt-get install -y curl git openssl software-properties-common wget make python3-dev python3-venv binutils jq
 }
 
-install_geth() {
-    add-apt-repository -y ppa:ethereum/ethereum
-    apt-get update 
-    apt-get install -y ethereum
-}
-
 copy_ethereum_dir() {
     cp -r ./ethereum /home/
-}
-
-init_geth() {
-    geth init --datadir "$EXEC_DIR/datadir" "$EXEC_DIR/genesis.json"
 }
 
 prompt_for_ip() {
@@ -48,9 +38,7 @@ get_primary_enode() {
 
 setup() {
     install_dependencies
-    install_geth
     copy_ethereum_dir
-    init_geth
     prompt_for_ip
     get_primary_enode
 }
